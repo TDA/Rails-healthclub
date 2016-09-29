572a77ea60942a35f8ceb5ee98af926a1e33478e
Sai Chandramouli <schand31@asu.edu>
2016-09-29 00:43:34 -0700

    added validations

--- app/models/client_workout.rb
+++ app/models/client_workout.rb
@@ -1,2 +1,6 @@
 class ClientWorkout < ActiveRecord::Base
+  validates :client_name, presence: true
+  validates :trainer, presence: true
+  validates :duration_mins, presence: true
+  validates :paid_amount, numericality: {message: "%{value} is not a number!"}
 end
